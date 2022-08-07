f392633be69a2f28aa3adfb87113f10212ba9da1
laplas <nitwel95@gmail.com>
2022-08-07 21:01:40 +0300

    регистрация и авторизация

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.use(require("./user.route"));
 router.use(require("./notes.route"));
+router.use(require('./authRouter'))
 
 module.exports = router;
