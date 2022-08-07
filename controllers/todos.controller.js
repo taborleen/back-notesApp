4494dfce601f0915fb5fe843a4b1a08fa84ec944
Denu <Denu@dota2.ru>
2022-08-07 20:53:59 +0300

    все крад для todos and notes

--- controllers/todos.controller.js
+++ controllers/todos.controller.js
@@ -3,7 +3,7 @@ const Todo = require("../models/todos.model");
 module.exports.todosController = {
   postTodo: async (req, res) => {
     try {
-      const { name, title, time, color, important } = req.body;
+      const { name, title, time, color, important, completed } = req.body;
 
       const todic = await Todo.create({
         name,
@@ -11,6 +11,7 @@ module.exports.todosController = {
         time,
         color,
         important,
+        completed
       });
 
       return res.json(todic);
@@ -47,6 +48,24 @@ module.exports.todosController = {
     } catch (error) {
       return res.status(400).json({
         error: "Ошибка при удалении: " + error.message,
+      });
+    }
+  },
+  pathTodo: async (req, res) => {
+    try {
+      await Todo.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        title: req.body.title,
+        time: req.body.time,
+        color: req.body.color,
+        important: req.body.important,
+        completed: req.body.completed
+      });
+      const todic = await Todo.findById(req.params.id);
+      return res.json(todic);
+    } catch (error) {
+      return res.status(400).json({
+        error: "Ошибка при изменении: " + error.message,
       });
     }
   },
--- routes/todo.route.js
+++ routes/todo.route.js
@@ -7,6 +7,7 @@ router.post("/todo/post", todosController.postTodo)
 router.get("/todo/get/:id", todosController.getTodoById)
 router.get("/todo/get", todosController.getAllTodo)
 router.delete("/todo/delete/:id", todosController.deleteTodo)
+router.patch("/todo/path/:id", todosController.pathTodo)
 
 
 module.exports = router;
